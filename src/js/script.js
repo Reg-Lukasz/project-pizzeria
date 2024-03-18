76ec459d3572863f4d4f6116938a8c3b4e9eb50d
Lukasz Reglinski <lukasz.reglinski@gmail.com>
2024-03-18 12:35:09 +0100

    Change code in script to fix accordion function

--- src/js/script.js
+++ src/js/script.js
@@ -92,10 +92,10 @@ const select = {
         event.preventDefault();
 
         /* find active product (product that has active class) */
-        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if(activeProduct && activeProduct != thisProduct.element){
+        if(activeProduct != null && activeProduct != thisProduct.element){
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
         
