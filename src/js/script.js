ec38bb7fe055a6073f256df2fcf59ce12c5a701b
Lukasz Reglinski <lukasz.reglinski@gmail.com>
2024-03-21 15:42:26 +0100

    Add code in script to create function toggling cart

--- src/js/script.js
+++ src/js/script.js
@@ -231,8 +231,8 @@
     constructor(element){
       const thisWidget = this;
 
-      console.log('Amount widget:', thisWidget);
-      console.log('constructor arguments:', element);
+      // console.log('Amount widget:', thisWidget);
+      // console.log('constructor arguments:', element);
 
       thisWidget.getElements(element);
       thisWidget.valueValidate();
@@ -296,6 +296,37 @@
     }
   }
 
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+      
+      console.log('new Cart', thisCart)
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      })
+
+    }
+  }
+
   const app = {
     initData: function(){
       const thisApp = this;
@@ -313,6 +344,13 @@
       }
     },
 
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       // console.log('*** App starting ***');
@@ -323,6 +361,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
